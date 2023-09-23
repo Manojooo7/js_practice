c69ab13c6e9d39a31da1f3dabd8199b6f09849b0
Manoj Mondal <manojmondal9834@gmail.com>
2023-09-23 17:21:51 +0000

    string

--- string.js
+++ string.js
@@ -88,15 +88,15 @@ the object is converted back to a regular string primitive.
 and this process is called boxing.
 */
 
-console.log(airline.toLowerCase());
-console.log(airline.toUpperCase());
+// console.log(airline.toLowerCase());
+// console.log(airline.toUpperCase());
 
 // fix captilization in name
 const passenger = 'jOnAs'; //Jonsa
 const passengerLower = passenger.toLowerCase()
 const passengerCorrect = passengerLower[0].toUpperCase() + passengerLower//.slice(1)
 
-console.log(passengerCorrect); 
+// console.log(passengerCorrect); 
 
 // comparing email
 
@@ -107,36 +107,144 @@ const loginEmail = ' Hello@Jonas.Io \n';
 const lowerEmail = loginEmail.toLowerCase()
 const trimmedEmail = loginEmail.toLowerCase().trim()
 
-console.log(email === trimmedEmail);
+// console.log(email === trimmedEmail);
 
-console.log(trimmedEmail);
+// console.log(trimmedEmail);
 
 // replacing
 
 const priceGB = '$288,97';
 const priceIN = priceGB.replace('$', '₹').replace(',','.');
 
-console.log(priceIN);
+// console.log(priceIN);
 
 const annuuncemnet = "All pasenger come to boarding door 23. Boarding door 23!";
 
-console.log(annuuncemnet.replaceAll('door', 'gate'));
+// console.log(annuuncemnet.replaceAll('door', 'gate'));
 
 //Bolleans
 const plane1 = 'Airbus A320neo';
 
-console.log(plane1.includes('A320'));
-console.log(plane1.includes('Boeing'));
-console.log(plane1.startsWith('A'));
+// console.log(plane1.includes('A320'));
+// console.log(plane1.includes('Boeing'));
+// console.log(plane1.startsWith('A'));
 
-plane1.startsWith('Airbus') && plane1.endsWith('neo')? console.log('part of the new Airbus family'): console.log('Not from the family');
+// plane1.startsWith('Airbus') && plane1.endsWith('neo')? console.log('part of the new Airbus family'): console.log('Not from the family');
 
 const chceckBagage = function(item){
 
     const bag = item.toLowerCase()
 
-    bag.includes('knife')? console.log('need to check this bag'): console.log('no need to check');
+    // bag.includes('knife')? console.log('need to check this bag'): console.log('no need to check');
 }
 
 chceckBagage('i have some food, water bottle and a pocket Knife')
 chceckBagage('i have a laptop, a camera')
+
+// Split and Join Method
+
+// below we split this entire string by '+'
+console.log('a+very+nice+string'.split('+'));
+
+// below we split this entire string by empty space ' '
+console.log('Manoj Mondal'.split(' '));
+
+const [firstName, lastName] = 'Manoj Mondal'.split(' ');
+
+// joining
+const newName=['Mr.', firstName, lastName.toUpperCase()].join(' ')
+
+console.log(newName);
+
+const capitalizedName = function(name){
+    // here we split the name by space
+    const names =name.split(' ');
+    const namesUpper =[]
+
+    for(const n of names){
+        
+        /* below we first convert the first letter of the name
+        to uppercase eith the toUpperCase() methode then we slcie
+        the name from the 2nd word and join it with the capital word
+
+        manoj mondal>{ M M > anoj ondal>} the  we add this twho string 
+        which convert to Manoj Mondal 
+        
+        */
+
+        // namesUpper.push(n[0].toUpperCase()+n.slice(1))
+
+        // here we replace the small word of the first sentece 
+        //with capital captial word 
+
+        namesUpper.push(n.replace(n[0], n[0].toUpperCase()))
+    }
+
+    console.log(namesUpper.join(' '));
+}
+
+capitalizedName('manoj mondal')
+capitalizedName('roshni mallick')
+capitalizedName('subham rai')
+
+// padding
+
+const message = 'go to gate 23!';
+
+// this padStart() methode wil convert the length of the 
+//mesege to 25 by adding + at start
+
+// and the padEnd method will add some + at the end of the string 
+// until the length of the string go up to 35
+console.log(message.padStart(25, '+').padEnd(35, '+'));
+
+// real world example
+
+/*
+This is an real world exapmle we usallly saw that every in online platform we never see the 
+full numbers of the credit card rather we only saw the lat 4 digit so where we create a function
+maskCreditCard which do the same
+
+*/
+
+const maskCreditCaard = function(number){
+    // here weconver the argument from number to string
+    const str = number + '';
+
+    // here we slice  the the string from last 4 digit by using negetive value
+    const last = str.slice(-4);
+
+    // and here we add padding of * with the sliced 4 digit upto it's original length
+    return last.padStart(str.length, '*')
+
+}
+
+console.log(maskCreditCaard(4566522))
+console.log(maskCreditCaard(4566522244566324))
+console.log(maskCreditCaard('485662225845552'))
+
+
+// repeat method
+
+const message2 = 'Bad Weather ......... all departre Delayed';
+
+console.log(message.repeat(5));
+
+const planesInLine = function(n){
+    console.log(`There are ${n} plans in line ${'✈'.repeat(n)}`);
+}
+
+planesInLine(5)
+planesInLine(12)
+planesInLine(18)
+
+
+
+
+
+
+
+
+
+
+
